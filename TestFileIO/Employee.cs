c063cb0e62d75484cfd2df78546bd8535b841fd7
Mary Olive <mary.olive@me.com>
2016-11-10 11:20:18 -0500

    First commit

---
+++ TestFileIO/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFileIO
+{
+    class Employee
+    {
+        private string Name;
+        private float Salary;
+
+        public Employee(string name, float salary)
+        {
+            Name1 = name;
+            Salary1 = salary;
+        }
+
+        public string Name1
+        {
+            get
+            {
+                return Name;
+            }
+
+            set
+            {
+                Name = value;
+            }
+        }
+
+        public float Salary1
+        {
+            get
+            {
+                return Salary;
+            }
+
+            set
+            {
+                Salary = value;
+            }
+        }
+    }
+}
--- TestFileIO/Program.cs
+++ TestFileIO/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,47 @@ namespace TestFileIO
     {
         static void Main(string[] args)
         {
+
+            List<Employee> EmployeeList = ReadDataFile();
+
+            ///////////////////////
+            foreach (Employee Emp in EmployeeList)
+            {
+                // Console.WriteLine(Emp.Name1 + "=>" + Emp.Salary1);              //3 ways to print to console
+                //Console.WriteLine($"Name: {Emp.Name1}, Salary:${Emp.Salary1}");
+                Console.WriteLine("Name: {0}, Salary: {1}", Emp.Name1, Emp.Salary1);
+            }
+            WriteToDataFile("Q","100000");
+
+        }
+
+        private static void WriteToDataFile(string Name, string salary)
+        {
+            StreamWriter writer = new StreamWriter("../../DataFile.txt", true);
+            writer.Write($"\n{Name},{salary}");
+            writer.Close();
+        }
+
+        private static List<Employee> ReadDataFile()
+        {
+            List<Employee> EmployeeList = new List<Employee>();
+            string filelocation = "../../DataFile.txt";
+
+            StreamReader reader = new StreamReader(filelocation);
+
+            //Console.WriteLine(reader.ReadToEnd());
+
+            string Data = reader.ReadToEnd().Trim().Trim('\n');
+            string[] Records = Data.Split('\n');
+
+            foreach (string record in Records)
+            {
+                string[] rc = record.Split(',');
+                EmployeeList.Add(new Employee(rc[0],float.Parse(rc[1])));
+            }
+            reader.Close();
+
+            return EmployeeList;
         }
     }
 }
